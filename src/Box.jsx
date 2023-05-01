b0acaa441b6a0ca62eacc85c2dc2891b88d5668f
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-01 20:49:11 +0300

    upsated jsx

--- src/Box.jsx
+++ src/Box.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Box() {
     return (
-        <div className="Box">
+        <div className="box">
             <h1 className="box--value"></h1>
         </div>
     )
