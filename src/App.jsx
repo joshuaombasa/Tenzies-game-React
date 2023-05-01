91978b59da038b5d9c39ee2d60661aaa9ec92fb7
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-01 20:49:46 +0300

    upsated jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 
+import Box from './Box'
+
 import './App.css'
 
 function App() {
   
 
   return (
-    <div>
-      
+    <div className='container'>
+      <div className="boxes--container">
+        <h1 className="title">Tenzies</h1>
+        <h4 className="game--details">
+        Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+        </h4>
+        <div className="buttons--cotainer">
+       
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
+      </div>
     </div>
   )
 }
