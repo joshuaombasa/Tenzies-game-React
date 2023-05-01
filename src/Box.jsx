f5d0140f71ae7b090a45ac63913d59cbb8c48f7e
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-01 23:38:29 +0300

    updated jsx

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 export default function Box(props) {
+
+    const styles = {backgroundColor: props.isHeld? "green" : ""}
+
     return (
-        <div className="box" onClick={() => {props.toggleIsHeld(props.id)}}>
+        <div style={styles} className="box" onClick={() => {props.toggleIsHeld(props.id)}}>
             <h1 className="box--value">{props.value}</h1>
         </div>
     )
